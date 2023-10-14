1cc47364f5ff1586c89c23044a6164d5e89cc9ff
Mayank Bhardwaj <145973900+beingmayank@users.noreply.github.com>
2023-10-14 19:04:18 +0530

    Add files via upload

---
+++ voice_assistant.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk1():
+    engine.say('I am your voice assistant')
+    engine.say('What can I do for you')
+    engine.runAndWait()
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+talk1()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+    except:
+        pass
+    return command
+
+def run_voice_assistant():
+    command = take_command()
+    # print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    else:
+        print('Please say it again.')
+        talk('Please say it again.')
+
+while True:
+    run_voice_assistant()
+
